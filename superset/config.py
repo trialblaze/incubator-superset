d558d557a476a49b5f221b4e776cde2f350e9a27
Ramtin Khalatbari <ramtin.kalatbari@trialblaze.com>
2019-12-11 20:04:32 +0330

    Update app name

--- superset/config.py
+++ superset/config.py
@@ -156,7 +156,7 @@ PROXY_FIX_CONFIG = {"x_for": 1, "x_proto": 1, "x_host": 1, "x_port": 1, "x_prefi
 # GLOBALS FOR APP Builder
 # ------------------------------
 # Uncomment to setup Your App name
-APP_NAME = "Superset"
+APP_NAME = "TrialBlaze Analytics"
 
 # Uncomment to setup an App icon
 APP_ICON = "/static/assets/images/logo-original-horizontal@2x.png"
